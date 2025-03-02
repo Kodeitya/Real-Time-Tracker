81d836e18e7ffa2821a5917610238482ed23ab2f
Aditya Kumar <kumaraditya0511@gmail.com>
2025-03-02 23:43:01 +0530

    fixed port

--- app.js
+++ app.js
@@ -28,7 +28,13 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
+// server.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// });
+
+
+const PORT = process.env.PORT || 8080; // Use Railway-assigned port or default to 8080
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
